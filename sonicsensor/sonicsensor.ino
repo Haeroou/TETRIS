2990864e5ad9a443b48a41f8c7344345b4c2e6f4
Ke_Vini <67322652+Haeroou@users.noreply.github.com>
2024-12-13 21:14:43 +0900

    add file sonicsensor.ino

---
+++ sonicsensor/sonicsensor.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+SoftwareSerial s(2,3);  //2:RX 3:TX
+
+int trigPin = 13;
+int echoPin = 12;
+void setup() {
+  Serial.begin(9600);
+  s.begin(9600);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+}
+
+void loop() {
+  long duration, distance, temp_distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  temp_distance = ((float)(340 * duration) / 10000) / 2;
+
+  if (temp_distance <= 35) {
+    distance = temp_distance;
+  }
+
+  if (distance <= 10) {
+    s.write('0');
+  }
+  else if (distance <= 22) {
+    s.write('1');
+  }
+  else {
+    s.write('2');
+  }
+
+  delay(100);
+}
